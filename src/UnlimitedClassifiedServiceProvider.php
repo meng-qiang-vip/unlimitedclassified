be6d0207ec16d38640da385cb5c1eda8c606282f
mengqiang <meng_qiang_vip@qq.com>
2022-12-30 17:41:58 +0800

    初始化项目

---
+++ src/UnlimitedClassifiedServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MengQiang\UnlimitedClassified;
+
+use Illuminate\Support\ServiceProvider;
+
+class UnlimitedClassifiedServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        $this->app->singleton('unlimited_classified', function () {
+            return new UnlimitedClassified();
+        });
+    }
+}
